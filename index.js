1b3bad9c72b9486bfde6f104ba6b2a8eaad3c4f1
mitsunaga870 <makingacounts4159@gmail.com>
2022-01-14 04:54:00 +0900

    ver 0.5 ウィンドウサイズ調整の雛形 時間指定URLだとエラーウィンドウが作成される不具合を解消

--- index.js
+++ index.js
@@ -2,21 +2,37 @@ var scan =require("./scan");
 var openurl = require("./openurl");
 var convert = require("./urlcomberter");
 
+let amount = 0;
 main()
 
 function main() {
-    readwindws()
-        .then(function (data){//窓数処理後１
-            console.log("URLを入力してください")
-            scan(data)//URL入力
-                .then(function (data){
-                    convert(data)//URL変換
-                        .then(function (data){
-                            openurl(data)//開く
-                        })
-                })
-        });
+    var swich = true;
+    console.log("ウィンドウサイズを変更しない場合はNを入力")
+    scan()
+        .then(function (data){
+            if (data=="N"){swich=false;}
+            makewindow()
+                .then(function (){
 
+                });
+        })//ウィンドウ変更するか確認
+
+}
+function makewindow(){
+    return new Promise((resolve)=>{
+        readwindws()
+            .then(function (data){//窓数処理後１
+                console.log("URLを入力してください")
+                amount=data;
+                scan(data)//URL入力
+                    .then(function (data){
+                        convert(data)//URL変換
+                            .then(function (data){
+                                openurl(data);//開く
+                            })
+                    })
+            });
+    })
 }
 function readwindws(){//窓数を聞く
     return new Promise((resolve) => {
