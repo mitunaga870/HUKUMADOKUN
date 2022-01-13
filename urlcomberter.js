0c093d9bb60aa197cbf38b9de47ea10a58485775
mitsunaga870 <makingacounts4159@gmail.com>
2022-01-14 04:55:16 +0900

    ver 0.5 ウィンドウサイズ調整の雛形 時間指定URLだとエラーウィンドウが作成される不具合を解消

--- urlcomberter.js
+++ urlcomberter.js
@@ -1,7 +1,7 @@
 module.exports=function combert(url){
     return new Promise((resolve)=> {
         for (let i = 0; i < url.length; i++) {
-            let id = url[i].split("v=")[1];
+            let id = url[i].split(/v=|&t/)[1];
             url[i] = "https://www.youtube.com/embed/" + id;
         }
         resolve(url);
